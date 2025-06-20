4fc15a35643c5face1347016a33be77cff1ae1c4
PC1 <bja.avataratechnobiz@gmail.com>
2025-06-20 18:23:13 +0530

    done

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,         // mysql.railway.internal
-  user: process.env.DB_USER,         // root
-  password: process.env.DB_PASS,     // your password
-  database: process.env.DB_NAME,     // railway
-  port: process.env.DB_PORT          // 3306
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
